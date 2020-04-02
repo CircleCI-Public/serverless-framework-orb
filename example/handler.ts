16090fb3db8669cd7f1c52ffd968db8fbf3897ae
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-04-02 17:44:10 -0400

    stringify event logging

--- example/handler.ts
+++ example/handler.ts
@@ -1,11 +1,11 @@
 function addNumbers(numbers: Array<number>) {
-  console.log("CloudWatch: numbers: \n" + numbers)
+  console.log("CloudWatch: numbers: \n", numbers)
   const reducer = (accumulator: number, currentValue: number) => accumulator + currentValue
   return numbers.reduce(reducer)
 }
 
 module.exports.add = async (event: any) => {
-  console.log("Full event: \n" + event)
+  console.log("Full event: \n", JSON.stringify(event))
   const response = {
     statusCode: 200,
     body: JSON.stringify(addNumbers(event.body.numbers))
