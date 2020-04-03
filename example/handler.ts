9152d556d6b3797ea80ee1c3244fb9dd59ce29f2
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-04-03 14:39:33 -0400

    Remove array. Fails to parse in API Gateway.

--- example/handler.ts
+++ example/handler.ts
@@ -1,23 +1,14 @@
-function addNumbers(numbers: Array<number>) {
-  console.log("CloudWatch: numbers: \n", numbers)
-  const reducer = (accumulator: number, currentValue: number) => accumulator + currentValue
-  return numbers.reduce(reducer)
+function addNumbers(numA: number, numB: number) {
+  return numA + numB
 }
 
 module.exports.add = async (event: any) => {
-  // https://stackoverflow.com/a/52240132/8963385
-  // Body is being processed as a string, not json, ONLY on API gateway which is failing to properly insert into addNumbers.
-  const responseBody = JSON.stringify(event.body)
-  console.log("Stringified Body: \n", responseBody)
-  const parsedBody = JSON.parse(responseBody)
-  console.log("TypeOf ParsedBody: \n", typeof(parsedBody))
-  console.log("Parsed Body: \n", parsedBody)
-  const userNumbers = parsedBody.numbers
-  console.log("TypeOf userNumbers: \n", typeof(userNumbers))
-  console.log("Parsed Body: \n", userNumbers)
+  const eventString = JSON.stringify(event.body)
+  const numbers = JSON.parse(eventString)
+  console.log("Numbers: ", numbers)
   const response = {
     statusCode: 200,
-    body: JSON.stringify(addNumbers(userNumbers))
+    body: JSON.stringify(addNumbers(numbers.numA, numbers.numB))
   }
   return response
 }
