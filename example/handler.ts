f64b91d1a7130e1480cdb4f074e5d9713bc7b229
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-04-03 14:53:57 -0400

    more reporting to cloudwatch

--- example/handler.ts
+++ example/handler.ts
@@ -6,6 +6,8 @@ module.exports.add = async (event: any) => {
   const eventString = JSON.stringify(event.body)
   const numbers = JSON.parse(eventString)
   console.log("Numbers: ", numbers)
+  console.log("Type of Numbers: ", typeof(numbers))
+  console.log(numbers.numA, numbers.numB)
   const response = {
     statusCode: 200,
     body: JSON.stringify(addNumbers(numbers.numA, numbers.numB))
