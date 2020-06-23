ed319ff3cf04b9fb22b7c457c66656e8a5560bb3
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-06-23 09:42:12 -0400

    fix jest test from previous change

--- example/tests/add.spec.ts
+++ example/tests/add.spec.ts
@@ -3,7 +3,7 @@ const addFunction = require("../handler").add
 describe("add function", () => {
     test("it should add all numbers passed via array", async () => {
         let input = require("../sampleData/data.json")
-        let output = {"body.result": "15", "statusCode": 200}
+        let output = {"body": {"result" : "15"}, "statusCode": 200}
         expect(await addFunction(input)).toEqual(output)
     })
 })
