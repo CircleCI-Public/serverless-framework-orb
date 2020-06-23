3deb323710c71a36002474248c8e017a76ce9760
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-06-23 10:23:53 -0400

    array errors. Stashing changes (as a commit)

--- example/handler.ts
+++ example/handler.ts
@@ -1,10 +1,24 @@
 function addNumbers(numbers: Array<number>) {
+  console.log("Adding Numbers")
+  console.log("Numbers to add: " + numbers)
+  console.log(typeof numbers)
   return numbers.reduce((acc,cv) => acc + cv)
 }
 
 module.exports.add = async (event: any) => {
  //Works locally only. Has a problem behind the API Gateway
   let bodyObj = event.body
+  let values = bodyObj.numbers.values()
+  console.log("Values = " + values.toString())
+  console.log("Received Add event")
+  console.log("Body of event: " + bodyObj)
+  console.log(typeof bodyObj)
+  console.log("Numbers: " + bodyObj.numbers)
+  console.log("Number type: " + typeof bodyObj.numbers)
+  console.log("Stringified version:")
+  console.log(JSON.stringify(bodyObj))
+  console.log("-----")
+  console.log()
   const response = {
     statusCode: 200,
     body: {result: `${addNumbers(bodyObj.numbers)}`}
