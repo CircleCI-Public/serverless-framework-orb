c34359ee48c256139c361cc4347b10dce09a258f
Kyle Tryon <33272306+KyleTryon@users.noreply.github.com>
2020-06-26 11:32:53 -0400

    [semver:skip] Sample Project Example - AWS Proxy Gateway Fix (#3)
    * change tests for local testing
    
    * stringify result for API gateway

--- example/handler.ts
+++ example/handler.ts
@@ -1,21 +1,34 @@
 function addNumbers(numbers: Array<number>) {
+  console.log("Adding Numbers")
+  console.log("Numbers to add: " + numbers)
   return numbers.reduce((acc,cv) => acc + cv)
 }
 
 module.exports.add = async (event: any) => {
- //Works locally only. Has a problem behind the API Gateway
-  let bodyObj = event.body
+
+  if (event.body) {
+    console.log("Running on AWS")
+    let body = JSON.parse(event.body)
+    var numbers = body.numbers
+  } else if (event.numbers) {
+    console.log("Running Locally")
+    var numbers = event.numbers
+  } else {
+    console.log ("ERROR IN EVENT")
+  }
   const response = {
     statusCode: 200,
-    body: `${addNumbers(bodyObj.numbers)}`
+    body: JSON.stringify({result: `${addNumbers(numbers)}`})
   }
+  console.log("Resonse: ")
+  console.log(response)
   return response
 }
 
 module.exports.hello = async (event: any) => {
   const response = {
     statusCode: 200,
-    body: "Hello!"
+    body: JSON.stringify({ message: "Hello!" })
   }
   console.log({ response })
   return response
