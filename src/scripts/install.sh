e9fe96f75f8b0c6b8edaddd0be20a91bcae7778c
Kyle T <33272306+KyleTryon@users.noreply.github.com>
2022-04-25 15:11:03 -0400

    feat: set download mirror location default to us (#33)
    * feat: add mirror parameter

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-curl -o- -L https://slss.io/install | VERSION=$ORB_PARAM_SERVERLESS_VERSION bash
+export SLS_SCRIPT_URL="https://sls-standalone-sv-1300963013.cos.na-siliconvalley.myqcloud.com/install.sh"
+SLS_GEO_LOCATION=us
+if [ "$ORB_PARAM_MIRROR" = "cn" ]; then
+  export SLS_SCRIPT_URL="https://sls-standalone-1300963013.cos.ap-shanghai.myqcloud.com/install.sh"
+  export SLS_GEO_LOCATION=cn
+fi
+curl -o- -L "$SLS_SCRIPT_URL" | VERSION=$ORB_PARAM_SERVERLESS_VERSION bash
 # shellcheck disable=SC2016
 echo 'export PATH=$HOME/.serverless/bin:$PATH' >> "$BASH_ENV"
 # shellcheck disable=SC1090
