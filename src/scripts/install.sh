3d6f9385c80400058b7121cebe6291ca21e1fbc0
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-12-13 20:08:18 -0800

    fix: switch the mirror selection responsibility to serverless

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export SLS_SCRIPT_URL="https://sls-standalone-sv-1300963013.cos.na-siliconvalley.myqcloud.com/install.sh"
-SLS_GEO_LOCATION=us
-if [ "$ORB_PARAM_MIRROR" = "cn" ]; then
-  export SLS_SCRIPT_URL="https://sls-standalone-1300963013.cos.ap-shanghai.myqcloud.com/install.sh"
-  export SLS_GEO_LOCATION=cn
-fi
-curl -o- -L "$SLS_SCRIPT_URL" | VERSION=$ORB_PARAM_SERVERLESS_VERSION bash
+
+[ "$ORB_PARAM_MIRROR" = "cn" ] && locale=cn
+[ "$ORB_PARAM_MIRROR" = "us" ] && locale=us
+
+# Let Serverless handle mirror selection.
+curl -o- -L https://slss.io/install | VERSION="$ORB_PARAM_SERVERLESS_VERSION" SLS_GEO_LOCATION="$locale" bash
+
 # shellcheck disable=SC2016
 echo 'export PATH=$HOME/.serverless/bin:$PATH' >> "$BASH_ENV"
 # shellcheck disable=SC1090
